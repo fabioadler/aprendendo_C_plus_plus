24ed950fa5d6f86349822aa661f1f7d991dca13a
Fábio Adler <adlerluna@hotmail.com>
2026-01-29 21:40:05 -0300

    atualização nos loops

--- 05 loop_for/for.cpp
+++ 05 loop_for/for.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
 
     cout << "Contagem regreciva" << endl;
-    for(int c = 10; c >= 0; c --){
+    for(int c = 10; c >= 0; c --){ //O for no C++ e em outras linguaguens precisa de uma variavel de referencia, uma condição e uma incrementação
         cout << "Contando: " << c << endl;
     }
     return 0;
--- 06 loop_while/while.cpp
+++ 06 loop_while/while.cpp
@@ -8,10 +8,18 @@ int main() {
 
     int num = 0;
     cout << "Contando de 0 a 100" << endl;
-    while(num<=100){
+    while(num<=100){ //O while só precisa de uma condição para funcionar, enquanto for verdadeiro ele continua o loop
         cout << "]==> " << num << endl;
         num ++;
     }
+    cout << "Terminamos de contar" << endl;
+    // O while pode ser feito assim também
+    int num2 = 0;
+    cout << "Contando de 0 a 10" << endl;
+    do{
+        cout << "]---> " << num2 << endl;
+        num2 ++;
+    }while(num2 <= 10);
     cout << "Terminamos de contar" << endl;
     return 0;
 

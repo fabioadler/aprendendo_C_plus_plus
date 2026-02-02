fdf0c668b43a0b457c605f50e0b2f2c1efc01bcd
Fábio Adler <adlerluna@hotmail.com>
2026-02-02 10:43:36 -0300

    update calculos

--- 04 operadores_aritimeticos/calculos.cpp
+++ 04 operadores_aritimeticos/calculos.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
 
     locale::global(locale(""));
-    double num1,num2,soma,sub,multi,divi,resto,elev;
+    double num1,num2,soma,sub,multi,divi,resto,elev,raiz;
     cout << "Seja bem vindo aos nossos resultados de calculo!" << endl;
     cout << "Digite o primeiro numero: ";
     cin >> num1;
@@ -21,12 +21,14 @@ int main() {
     divi = num1 / num2;
     resto = fmod(num1,num2);
     elev = pow(num1,num2);
+    raiz = pow(num1,1.0/num2);
     cout << "A soma e: " << soma << endl;
     cout << "A subtração e: " << sub << endl;
     cout << "A multiplicação e: " << multi << endl;
     cout << "A divisão e: " << divi << endl;
     cout << "O resto da divisão e: " << resto << endl;
-    cout << "A elevação e: " << elev << endl << endl;
+    cout << "A elevação e: " << elev << endl;
+    cout << "A Raiz e: " << raiz << endl << endl;
     cout << "Obrigado por usar o nosso sistema!" << endl;
     return 0;
 

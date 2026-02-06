a49a6c837ad86d676aef6aec946d3e90730989e4
Fábio Adler <adlerluna@hotmail.com>
2026-02-06 16:13:35 -0300

    Atualização input

--- 03 input/input.cpp
+++ 03 input/input.cpp
@@ -8,10 +8,13 @@ using namespace std;
 int main() {
 
     locale::global(locale(""));
-    cout << "Digit o seu nome: ";
-    char* nome = new char[100];
-    cin >> nome;
-    cout << "Seu nome e " << nome << endl;
+    string idade;
+    string nome;
+    cout << "Digite a sua idade: ";
+    getline(cin,idade); //O getline(cin,[variavel]) cerve para pegar todo conteudo da linha, por que o cin só pega até o primeiro espaço
+    cout << "Digite o seu nome: ";
+    getline(cin,nome);
+    cout << "Seu nome e " << nome << " e você tem " << idade << " anos" << endl;
     return 0;
 
 }
